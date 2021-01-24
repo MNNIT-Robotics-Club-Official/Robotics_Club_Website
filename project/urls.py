3a40768954e7ef6a531a353315966d47e67813e3
AnuragGupta806 <54318429+AnuragGupta806@users.noreply.github.com>
2021-01-24 16:26:13 +0530

    added avishkar page

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'project'
+
 urlpatterns=[
     path('',views.list,name='project_list'),
     path('detail/<int:pk>',views.detail,name='project_detail'),
