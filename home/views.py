088df203abdbb881b778c5f26cac968a05e79c3d
ishuraj2010@gmail.com <ishuraj2010@gmail.com>
2021-01-26 20:54:31 +0530

    donate page done

--- home/views.py
+++ home/views.py
@@ -14,5 +14,8 @@ def faculty(request):
 def gallery(request):
     return render(request, 'gallery.html')
 
+def donate(request):
+    return render(request, 'donate.html')
+
 def testimonial(request):
     return render(request, 'testimonial.html')
