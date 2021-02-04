b09369ae14008a4001e0a2c76964aeb6733034a3
Divyansh Upadhyay <leolovelucia@gmail.com>
2021-02-04 20:05:10 +0530

    user authority added

--- project/forms.py
+++ project/forms.py
@@ -4,4 +4,4 @@ from .models import Project
 class ProjectForm(forms.ModelForm):
     class Meta:
         model=Project
-        fields=['title','aim','detail','status','vidlink','github','members']
+        fields=['title','aim','detail','status','vidlink','github','members','tags']
