83aac6af8e7a322d30fdabe8419985e5bca05b5d
AnuragGupta806 <54318429+AnuragGupta806@users.noreply.github.com>
2021-02-13 17:54:32 +0530

    Added User Profile Page

--- user/forms.py
+++ user/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -9,3 +10,9 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        # fields = ['first_name','last_name','regnum','branch']
+        exclude = ('user','role')
--- user/urls.py
+++ user/urls.py
@@ -11,4 +11,6 @@ urlpatterns=[
     path('roles/',views.changerole,name='change_role'),
     path('compreq/',views.comprequest,name='component_request'),
     path('admin/',views.adminPage,name='admin_page'),
+    path('profileCreation/',views.userProfileCreation,name='profile_form'),
+    path('profile/',views.userProfile,name='profile_page'),
 ]
