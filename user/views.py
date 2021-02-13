562f67524e72fe1be1aebf56082505723ff60492
AnuragGupta806 <54318429+AnuragGupta806@users.noreply.github.com>
2021-02-13 17:54:32 +0530

    Added User Profile Page

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect,HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserProfileForm
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.models import User
 from component.models import Request
@@ -9,6 +9,8 @@ from django.template.loader import render_to_string
 from django.http import JsonResponse
 from component.models import Request
 from blog.models import Blog
+from project.models import Project
+from component.models import Component,Request
 from .models import Profile
 # Create your views here.
 
@@ -18,12 +20,15 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in')
-            return redirect('user:login_page')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request, f'Congratulations {username}!! Your account has been created!')
+            return redirect('home:index')
         else:
-            # messages.info(request, "Invalid Registration")
             for msg in form.error_messages:
                 messages.error(request, f"{msg}: {form.error_messages[msg]}")
+            return redirect('user:register_page')
     else:
         form = UserRegisterForm()
     return render(request, 'register.html', {'form': form})
@@ -89,3 +94,30 @@ def adminPage(request):
     context['requests']=Request.objects.filter(status=0)
     context['blogs']=Blog.objects.filter(approved=False)
     return render(request, 'user/admin_dashboard.html', context)
+
+def userProfileCreation(request):
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = Profile(user=request.user)
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST,instance=profile)
+        if form.is_valid():
+            profile.save()
+            messages.success(request, f'Your Profile has been updated')
+            return redirect('user:profile_page')        
+        else:
+            for msg in form.error_messages:
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
+            return redirect('user:profile_form')
+    else:
+        form = UserProfileForm()
+    return render(request, 'user/profile_form.html', {'form': form})
+
+@login_required
+def userProfile(request):
+    context = {}
+    context['blogs'] = Blog.objects.filter(author=request.user).order_by('approved')
+    context['projects'] = Project.objects.filter(members=request.user).order_by('status')
+    context['components'] = Request.objects.filter(request_user=request.user).order_by('status')
+    return render(request,'user/user_dashboard.html',context)
