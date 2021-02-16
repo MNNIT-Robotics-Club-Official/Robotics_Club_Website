c1849cf9fb34eaea90fb21dad7a3da7b9915a7d9
AnuragGupta806 <54318429+AnuragGupta806@users.noreply.github.com>
2021-02-16 16:30:38 +0530

    Modified Project

--- project/forms.py
+++ project/forms.py
@@ -4,4 +4,5 @@ from project.models import Project
 class ProjectForm(forms.ModelForm):
     class Meta:
         model=Project
-        fields=['title','aim','detail','status','vidlink','github','members','image','tags']
+        fields=['title','aim','detail','comp_and_tech','status','vidlink','github','members','image','tags']
+        
