4718212a0aa2b0a5b7fd110c9aa7d3fe1e8906f0
AnuragGupta <54318429+AnuragGupta806@users.noreply.github.com>
2021-02-17 21:41:03 +0530

    Update settings.py

--- RoboClub/settings.py
+++ RoboClub/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = '=-f%d%xd$snga=*)d!_cz1p&eid@o!ya^08#*2d5c(de-@9p)m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['roboticsmnnit.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
