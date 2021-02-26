fa8084a135f3af08987affcb9bf7740ede945a9e
Hardik Mittal <hardikmittal12@gmail.com>
2021-02-26 17:56:55 +0530

    Sponsor page modify

--- static/js/sponsor.js
+++ static/js/sponsor.js
@@ -1,24 +1,21 @@
-$(document).ready(function () {
-    $("#myCarousel").on("slide.bs.carousel", function (e) {
-        var $e = $(e.relatedTarget);
-        var idx = $e.index();
-        var itemsPerSlide = 4;
-        var totalItems = $(".carousel-item").length;
+$('#recipeCarousel').carousel({
+    interval: 10000
+})
 
-        if (idx >= totalItems - (itemsPerSlide - 1)) {
-            var it = itemsPerSlide - (totalItems - idx);
-            for (var i = 0; i < it; i++) {
-                // append slides to end
-                if (e.direction == "left") {
-                    $(".carousel-item")
-                        .eq(i)
-                        .appendTo(".carousel-inner");
-                } else {
-                    $(".carousel-item")
-                        .eq(0)
-                        .appendTo($(this).find(".carousel-inner"));
-                }
-            }
+$('.carousel .carousel-item').each(function () {
+    var minPerSlide = 4;
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < minPerSlide; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
         }
-    });
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
 });
