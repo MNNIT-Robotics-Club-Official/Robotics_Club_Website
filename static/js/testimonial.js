38592a15747aff2f3c10051031bf975bcc41624f
ishuraj2010@gmail.com <ishuraj2010@gmail.com>
2021-03-10 01:05:59 +0530

    testimonial done

--- static/js/testimonial.js
+++ static/js/testimonial.js
@@ -7,8 +7,40 @@ $(document).ready(function(){
         itemsTablet:[768,1],
         itemsMobile:[650,1],
         pagination:true,
+        mouseDrag :false,
         navigation:false,
-        mouseDrag : false,
+        slideSpeed:1000,
+        autoPlay:8000
+    });
+
+    $("#sponsor-slider").owlCarousel({
+        items:4,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[979,2],
+        itemsTablet:[768,1],
+        itemsMobile:[650,1],
+        pagination:true,
+        mouseDrag :false,
+        navigation:false,
+        slideSpeed:1000,
+        autoPlay:8000
+    });
+
+    $("#home-testimonial-slider").owlCarousel({
+        items:2,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[979,2],
+        itemsTablet:[768,1],
+        itemsMobile:[650,1],
+        pagination:true,
+        mouseDrag :false,
+        nav:true,
+        navigation:true,
+        navigationText: [
+                "<i class='icon-chevron-left icon-white'><</i>",
+                "<i class='icon-chevron-right icon-white'>></i>"
+              ],
+        navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
         slideSpeed:1000,
         autoPlay:8000
     });
