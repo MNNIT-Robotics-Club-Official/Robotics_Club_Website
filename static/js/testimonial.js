03693230cb70d5f2d7e40a688a604c7c3b277329
ishuraj2010@gmail.com <ishuraj2010@gmail.com>
2021-03-10 20:29:35 +0530

    featured projects added

--- static/js/testimonial.js
+++ static/js/testimonial.js
@@ -44,4 +44,23 @@ $(document).ready(function(){
         slideSpeed:1000,
         autoPlay:8000
     });
+
+    $("#projects-slider").owlCarousel({
+        items:3,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[979,2],
+        itemsTablet:[768,1],
+        itemsMobile:[650,1],
+        pagination:true,
+        mouseDrag :false,
+        nav:true,
+        navigation:true,
+        navigationText: [
+                "<i class='icon-arrow-left icon-white'><</i>",
+                "<i class='icon-arrow-right icon-white'>></i>"
+              ],
+        navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+        slideSpeed:1000,
+        autoPlay:8000
+    });
 });
