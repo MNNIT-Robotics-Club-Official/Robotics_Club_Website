23f81449f84b873b87d3470bb7c9430bf3b7a507
ishuraj2010@gmail.com <ishuraj2010@gmail.com>
2021-03-12 17:40:25 +0530

    contact form ui corrected

--- home/forms.py
+++ home/forms.py
@@ -1,19 +1,10 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name=forms.CharField(max_length=100,label='Your Name')
-    email = forms.EmailField(label='Email')
-    body = forms.CharField(min_length=10,max_length=1000,label='Message',widget=forms.Textarea(attrs={'rows':5,'col':2}))
-    mail_subject = forms.CharField(max_length=100, label='Subject')
+    name=forms.CharField(max_length=100,label='Your Name',widget=forms.TextInput(attrs={'placeholder': 'Your Name'}))
+    email = forms.EmailField(label='Email',widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    body = forms.CharField(min_length=10,max_length=1000,label='Message',widget=forms.Textarea(attrs={'rows':5,'col':2, 'placeholder':'Message'}))
+    subject = forms.CharField(max_length=100, label='Subject',widget=forms.TextInput(attrs={'placeholder': 'Subject'}))
 
-
-    def __init__(self, *args, **kwargs):
-
-        for field_name, field in self.fields.items():
-            if field_name in self._placeholders:
-                self.fields[field_name].widget.attrs['placeholder'] = \
-                self.fields[field_name].label
-
-        super(ContactForm, self).__init__(*args, **kwargs)
 
 
