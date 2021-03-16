145b644c19499b6a81d5c28352bc5a31cbdd46fc
AnuragGupta806 <54318429+AnuragGupta806@users.noreply.github.com>
2021-03-17 01:59:00 +0530

    4 sponser per page

--- static/js/sponsor.js
+++ static/js/sponsor.js
@@ -1,21 +1,33 @@
-$('#recipeCarousel').carousel({
-    interval: 5000
-})
+// $('#recipeCarousel').carousel({
+//     interval: 5000
+// })
 
-$('.carousel .carousel-item').each(function () {
-    var minPerSlide = 4;
-    var next = $(this).next();
-    if (!next.length) {
-        next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
+// $('.carousel .carousel-item').each(function () {
+//     var minPerSlide = 4;
+//     var next = $(this).next();
+//     if (!next.length) {
+//         next = $(this).siblings(':first');
+//     }
+//     next.children(':first-child').clone().appendTo($(this));
 
-    for (var i = 0; i < minPerSlide; i++) {
-        next = next.next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-        }
+//     for (var i = 0; i < minPerSlide; i++) {
+//         next = next.next();
+//         if (!next.length) {
+//             next = $(this).siblings(':first');
+//         }
 
-        next.children(':first-child').clone().appendTo($(this));
-    }
+//         next.children(':first-child').clone().appendTo($(this));
+//     }
+// });
+$("#sponserpage-slider").owlCarousel({
+    items:4,
+    itemsDesktop:[1000,4],
+    itemsDesktopSmall:[979,3],
+    itemsTablet:[768,2],
+    itemsMobile:[650,1],
+    pagination:true,
+    mouseDrag :false,
+    nav:false,
+    slideSpeed:1000,
+    autoPlay:false
 });
