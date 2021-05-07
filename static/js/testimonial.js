5213b3b7b2965fde38cce9909165ea93d5293706
Hardik-Mittal <hardikmittal12@gmail.com>
2021-05-07 10:50:32 +0530

    fix testimonial card height

--- static/js/testimonial.js
+++ static/js/testimonial.js
@@ -1,60 +1,60 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
     $("#testimonial-slider").owlCarousel({
-        items:2,
-        itemsDesktop:[1000,2],
-        itemsDesktopSmall:[979,2],
-        itemsTablet:[768,1],
-        itemsMobile:[650,1],
-        pagination:true,
-        mouseDrag :false,
-        navigation:false,
-        slideSpeed:1000,
-        autoPlay:8000
+        items: 2,
+        itemsDesktop: [1000, 2],
+        itemsDesktopSmall: [979, 2],
+        itemsTablet: [768, 1],
+        itemsMobile: [650, 1],
+        pagination: true,
+        mouseDrag: false,
+        navigation: false,
+        slideSpeed: 100000,
+        autoPlay: 800000
     });
-    
+
     $("#sponsor-slider").owlCarousel({
         items: 5,
-        itemsDesktop:[1000,3],
-        itemsDesktopSmall:[979,3],
-        itemsTablet:[768,2],
-        itemsMobile:[650,2],
-        pagination:true,
-        mouseDrag :false,
-        navigation:false,
-        slideSpeed:1000,
-        autoPlay:8000
+        itemsDesktop: [1000, 3],
+        itemsDesktopSmall: [979, 3],
+        itemsTablet: [768, 2],
+        itemsMobile: [650, 2],
+        pagination: true,
+        mouseDrag: false,
+        navigation: false,
+        slideSpeed: 100000,
+        autoPlay: 800000
     });
-    
+
     $("#home-testimonial-slider").owlCarousel({
-        items:2,
-        itemsDesktop:[1000,2],
-        itemsDesktopSmall:[979,2],
-        itemsTablet:[768,1],
-        itemsMobile:[650,1],
-        pagination:true,
-        mouseDrag :false,
-        nav:false,
-        navigation:false,
+        items: 2,
+        itemsDesktop: [1000, 2],
+        itemsDesktopSmall: [979, 2],
+        itemsTablet: [768, 1],
+        itemsMobile: [650, 1],
+        pagination: true,
+        mouseDrag: false,
+        nav: false,
+        navigation: false,
         // navigationText: [
-            //         "<i class='icon-arrow-left icon-white'><</i>",
+        //         "<i class='icon-arrow-left icon-white'><</i>",
         //         "<i class='icon-arrow-right icon-white'>></i>"
         //       ],
         // navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-        slideSpeed:1000,
-        autoPlay:8000
+        slideSpeed: 100000,
+        autoPlay: 800000
     });
 
     $("#projects-slider").owlCarousel({
-        items:3,
-        itemsDesktop:[1000,2],
-        itemsDesktopSmall:[979,2],
-        itemsTablet:[768,1],
-        itemsMobile:[650,1],
-        pagination:true,
-        mouseDrag :false,
-        nav:false,
-        slideSpeed:1000,
-        autoPlay:8000
+        items: 3,
+        itemsDesktop: [1000, 2],
+        itemsDesktopSmall: [979, 2],
+        itemsTablet: [768, 1],
+        itemsMobile: [650, 1],
+        pagination: true,
+        mouseDrag: false,
+        nav: false,
+        slideSpeed: 100000,
+        autoPlay: 800000
     });
 });
