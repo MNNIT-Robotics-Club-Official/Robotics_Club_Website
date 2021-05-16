ef45810e2f6b00c9bc652ce283e504f309146242
DuP-491 <divyansh.20198117@gmail.com>
2021-05-16 21:03:18 +0530

    changed email

--- RoboClub/settings.py
+++ RoboClub/settings.py
@@ -176,8 +176,8 @@ LOGOUT_REDIRECT_URL='home:index'
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'roboclubmnnit.test@gmail.com'
-EMAIL_HOST_PASSWORD = 'pzrxeipzequfuagr'
+EMAIL_HOST_USER = os.getenv('EMAIL')
+EMAIL_HOST_PASSWORD = os.getenv('PASSWORD')
 EMAIL_PORT = 587
 #django_heroku.settings(locals())
 
